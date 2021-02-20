01f3ef66ba84ec8945dc494d4e5e09e5eabed32c
Sadie Lombardi <slombardi@wpi.edu>
2021-02-20 18:34:14 -0500

    for kyle

---
+++ src/main/java/Controllers/DescController.java
@@ -0,0 +1,28 @@
+package Controllers;
+
+import javafx.scene.shape.Circle;
+import java.util.ArrayList;
+
+public class DescController {
+
+    /**
+     * Take in list of circles, get x and y coordinates
+     * then compare with user click location from mousepress event
+     *
+     * https://stackoverflow.com/questions/27999430/javafx-clickable-line-on-canvas
+     */
+    public void getMouseClick(MouseEvent t, Cirle array){
+
+        //go through all circles
+
+        //check with current mouse press data
+
+        //etc.
+        displayDescription(x, y);z
+    }
+
+    public displayDescription(int x, int y){
+
+    }
+
+}
