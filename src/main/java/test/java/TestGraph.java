a109182d5679260ac72e24613b0daf7c9c5cf611
Sadie Lombardi <slombardi@wpi.edu>
2021-02-20 15:06:18 -0500

    Add files via upload

---
+++ src/main/java/test/java/TestGraph.java
@@ -0,0 +1,13 @@
+//import GraphSystem.Graph;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestGraph {
+
+  //Graph graph;
+
+  @Test
+  public void testGetSize1() {
+    Assert.assertTrue(true);
+  }
+}
