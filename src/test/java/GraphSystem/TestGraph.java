56c1fec1c7c1dbd9e2680c1b9804cfd9664af2a2
smspry <smspry@wpi.edu>
2021-02-21 16:11:26 -0500

    mostly working add node button

--- src/test/java/GraphSystem/TestGraph.java
+++ src/test/java/GraphSystem/TestGraph.java
@@ -1,10 +1,7 @@
 package GraphSystem;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.LinkedList;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 // ^ JUST MAKING SURE YOU KNOW THE ABOVE IS NECESSARY FOR SHIT TO WORK
