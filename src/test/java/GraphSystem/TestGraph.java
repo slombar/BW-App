6c474cde9a896cd048f45eafc898bc36ab201e0f
ryandarcey <rtdarcey@wpi.edu>
2021-02-20 23:25:24 -0500

    aggressively commented on anything relating to tests properly working

--- src/test/java/GraphSystem/TestGraph.java
+++ src/test/java/GraphSystem/TestGraph.java
@@ -1,8 +1,10 @@
 // import GraphSystem.Graph;
 
+// copy these imports for all test classes (plus whatever else you may need obviously,
+//    but make sure it's the org.junit.jupiter.api....):
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+// ^ JUST MAKING SURE YOU KNOW THE ABOVE IS NECESSARY FOR SHIT TO WORK
 
 public class TestGraph {
 
