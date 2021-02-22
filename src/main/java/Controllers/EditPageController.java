ff3d1a592f92b5a2a64d03ad99e8471d55ddeaaa
smspry <smspry@wpi.edu>
2021-02-21 22:32:47 -0500

    brought all the databaseFunctionality back in

--- src/main/java/Controllers/EditPageController.java
+++ src/main/java/Controllers/EditPageController.java
@@ -280,24 +280,25 @@ public class EditPageController implements Initializable {
                 || listOfFields.get(7).getText().isEmpty()) {
               incompletePopup();
             } else {
-//                      DatabaseFunctionality.addNode(listOfFields.get(0).getText(),
-//                              listOfFields.get(1).getText(),
-//                              listOfFields.get(2).getText(),
-//                              listOfFields.get(3).getText(),
-//                              listOfFields.get(4).getText(),
-//                              listOfFields.get(5).getText(),
-//                              listOfFields.get(6).getText(),
-//                              listOfFields.get(7).getText(),
-//                              "O");
+              DatabaseFunctionality.addNode(
+                  listOfFields.get(0).getText(),
+                  listOfFields.get(1).getText(),
+                  listOfFields.get(2).getText(),
+                  listOfFields.get(3).getText(),
+                  listOfFields.get(4).getText(),
+                  listOfFields.get(5).getText(),
+                  listOfFields.get(6).getText(),
+                  listOfFields.get(7).getText(),
+                  "O");
 
-              System.out.println(listOfFields.get(0).getText());
-              System.out.println(listOfFields.get(1).getText());
-              System.out.println(listOfFields.get(2).getText());
-              System.out.println(listOfFields.get(3).getText());
-              System.out.println(listOfFields.get(4).getText());
-              System.out.println(listOfFields.get(5).getText());
-              System.out.println(listOfFields.get(6).getText());
-              System.out.println(listOfFields.get(7).getText());
+              //              System.out.println(listOfFields.get(0).getText());
+              //              System.out.println(listOfFields.get(1).getText());
+              //              System.out.println(listOfFields.get(2).getText());
+              //              System.out.println(listOfFields.get(3).getText());
+              //              System.out.println(listOfFields.get(4).getText());
+              //              System.out.println(listOfFields.get(5).getText());
+              //              System.out.println(listOfFields.get(6).getText());
+              //              System.out.println(listOfFields.get(7).getText());
               addNodeDialog.close();
               stackPane.toBack();
               popUp = false;
@@ -375,9 +376,9 @@ public class EditPageController implements Initializable {
             } else if (!nodeIDColData.contains(listOfFields.get(0).getText())) {
               nonexistantPopup();
             } else {
-//               DatabaseFunctionality.deleteNode(listOfFields.get(0).getText());
+              DatabaseFunctionality.deleteNode(listOfFields.get(0).getText());
 
-              System.out.println(listOfFields.get(0).getText());
+              //              System.out.println(listOfFields.get(0).getText());
               deleteNodeDialog.close();
               stackPane.toBack();
               popUp = false;
@@ -459,13 +460,14 @@ public class EditPageController implements Initializable {
                 || listOfFields.get(2).getText().isEmpty()) {
               incompletePopup();
             } else {
-              // DatabaseFunctionality.addEdge(listOfFields.get(0).getText(),
-              // listOfFields.get(1).getText(),
-              // listOfFields.get(2).getText());
+              DatabaseFunctionality.addEdge(
+                  listOfFields.get(0).getText(),
+                  listOfFields.get(1).getText(),
+                  listOfFields.get(2).getText());
 
-              System.out.println(listOfFields.get(0).getText());
-              System.out.println(listOfFields.get(1).getText());
-              System.out.println(listOfFields.get(2).getText());
+              //              System.out.println(listOfFields.get(0).getText());
+              //              System.out.println(listOfFields.get(1).getText());
+              //              System.out.println(listOfFields.get(2).getText());
               addEdgeDialog.close();
               stackPane.toBack();
               popUp = false;
@@ -544,9 +546,9 @@ public class EditPageController implements Initializable {
             } else if (!edgeIDColData.contains(listOfFields.get(0).getText())) {
               nonexistantPopup();
             } else {
-              // DatabaseFunctionality.deleteEdge(listOfFields.get(0).getText());
+              DatabaseFunctionality.deleteEdge(listOfFields.get(0).getText());
 
-              System.out.println(listOfFields.get(0).getText());
+              //              System.out.println(listOfFields.get(0).getText());
               deleteEdgeDialog.close();
               stackPane.toBack();
               popUp = false;
