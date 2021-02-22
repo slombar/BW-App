048b99013d575249a47747336929c20c9dae2ad7
Luke Hoy <lahoy@wpi.edu>
2021-02-22 01:48:08 -0500

    Updated sendSMS to function partially

--- src/main/java/Controllers/EmailPageController.java
+++ src/main/java/Controllers/EmailPageController.java
@@ -1,21 +1,16 @@
 package Controllers;
 
-import java.io.IOException;
-
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXSpinner;
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class EmailPageController {
@@ -60,5 +55,25 @@ public class EmailPageController {
     window.show();
   }
 
-  public void sendText(ActionEvent actionEvent) {}
+  public void sendText(ActionEvent actionEvent) throws IOException {
+
+    String phoneString = phoneNum.getText();
+    System.out.println(phoneString);
+
+    String home = System.getProperty("user.home");
+    String outputFile = home + "/Downloads/" + "mapImageThingy.png";
+
+    SharingFunctionality.sendSMS(phoneString, outputFile);
+    // phoneString, outputFile
+
+    System.out.println("Starting Up");
+    Parent parent = FXMLLoader.load(getClass().getResource("/Views/Submitted.fxml"));
+    Scene scene = new Scene(parent);
+    // this gets Stage info
+    Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    window.setTitle("Submitted");
+    // this sets the scene to the new one specified above
+    window.setScene(scene);
+    window.show();
+  }
 }
