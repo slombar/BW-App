325b27f5f8cf75e15b00f461a4eab8c27c786b7a
Kyle Lopez <kjlopez@wpi.edu>
2021-02-22 22:04:16 -0500

    getInfo for DBFunct

--- src/main/java/edu/wpi/teamO/Controllers/DatabaseFunctionality.java
+++ src/main/java/edu/wpi/teamO/Controllers/DatabaseFunctionality.java
@@ -596,4 +596,20 @@ public class DatabaseFunctionality {
       System.out.println("File is empty.");
     }
   }
+
+  public ArrayList<String> getInfo(String id) throws SQLException {
+    PreparedStatement pstmt = null;
+    pstmt = connection.prepareStatement("SELECT * FROM Description WHERE NODEID = '" + id +"'");
+    ResultSet rset = pstmt.executeQuery();
+    String title = rset.getString("title");
+    String toolText = rset.getString("toolText");
+    ArrayList<String> list = new ArrayList<String>();
+
+    //Adds title and text description of node to a list
+    list.add(title);
+    list.add(toolText);
+
+    return list;
+  }
+
 }
