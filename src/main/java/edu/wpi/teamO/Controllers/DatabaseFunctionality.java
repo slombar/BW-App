445608121fd5b28dc898f5a5ea97a6beeab3402b
Kyle Lopez <kjlopez@wpi.edu>
2021-02-22 22:36:49 -0500

    updated decription database

--- src/main/java/edu/wpi/teamO/Controllers/DatabaseFunctionality.java
+++ src/main/java/edu/wpi/teamO/Controllers/DatabaseFunctionality.java
@@ -597,19 +597,26 @@ public class DatabaseFunctionality {
     }
   }
 
-  public ArrayList<String> getInfo(String id) throws SQLException {
+  /**
+   * @param id
+   * @return ArrayList<String>
+   * @throws SQLException
+   */
+  public static ArrayList<String> getInfo(String id) throws SQLException {
     PreparedStatement pstmt = null;
-    pstmt = connection.prepareStatement("SELECT * FROM Description WHERE NODEID = '" + id +"'");
+    pstmt = connection.prepareStatement("SELECT * FROM Description WHERE NODEID = '" + id + "'");
     ResultSet rset = pstmt.executeQuery();
     String title = rset.getString("title");
     String toolText = rset.getString("toolText");
     ArrayList<String> list = new ArrayList<String>();
 
-    //Adds title and text description of node to a list
+    // Adds title and text description of node to a list
     list.add(title);
     list.add(toolText);
 
+    rset.close();
+    pstmt.close();
+
     return list;
   }
-
 }
