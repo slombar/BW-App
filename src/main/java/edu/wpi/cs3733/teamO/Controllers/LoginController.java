52cf7389c5a8b4b19b26e7a343bc2e11f4b17b07
smspry <smspry@wpi.edu>
2021-02-27 18:37:22 -0500

    added login page basics

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/LoginController.java
@@ -1,14 +1,32 @@
 package edu.wpi.cs3733.teamO.Controllers;
 
+import edu.wpi.cs3733.teamO.HelperClasses.PopupMaker;
 import edu.wpi.cs3733.teamO.Opp;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
-public class LoginController {
-  public void goToLogin(MouseEvent mouseEvent) {}
+public class LoginController implements Initializable {
+
+  @FXML private StackPane loginPane;
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    loginPane.setMaxSize(BorderPane.USE_PREF_SIZE, BorderPane.USE_PREF_SIZE);
+    loginPane.toBack();
+  }
+
+  public void goToLogin(MouseEvent mouseEvent) {
+    loginPane.toFront();
+    PopupMaker.incompletePopup(loginPane);
+  }
 
   public void goToMain(ActionEvent actionEvent) {
     try {
