ca17fbcbb357cc80c4c0aa408605698d0a86904f
Samuel Parks <sparks@wpi.edu>
2021-02-28 17:14:28 -0500

    more input scrubbing/popups

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/Popups/PatientSignInPopupController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/Popups/PatientSignInPopupController.java
@@ -5,9 +5,7 @@ import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.teamO.Database.UserHandling;
 import edu.wpi.cs3733.teamO.HelperClasses.PopupMaker;
 import edu.wpi.cs3733.teamO.Opp;
-
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,51 +15,48 @@ import javafx.scene.layout.StackPane;
 
 public class PatientSignInPopupController {
 
-    @FXML
-    private StackPane popupPane;
-    @FXML
-    private JFXTextField user;
-    @FXML
-    private JFXPasswordField pass;
+  @FXML private StackPane popupPane;
+  @FXML private JFXTextField user;
+  @FXML private JFXPasswordField pass;
 
-    @FXML
-    public void signIn(ActionEvent actionEvent) {
-        String username = user.getText();
-        String password = pass.getText();
-        if (username.equals("") || password.equals("")) {
-            PopupMaker.incompletePopup(popupPane);
-        } else {
-            try {
-                UserHandling.login(user.getText(), pass.getText());
-            } catch (Exception e) {
-                e.printStackTrace();
-                PopupMaker.invalidLogin(popupPane);
-            }
+  @FXML
+  public void signIn(ActionEvent actionEvent) {
+    String username = user.getText();
+    String password = pass.getText();
+    if (username.equals("") || password.equals("")) {
+      PopupMaker.incompletePopup(popupPane);
+    } else {
+      try {
+        UserHandling.login(user.getText(), pass.getText());
+      } catch (Exception e) {
+        e.printStackTrace();
+        PopupMaker.invalidLogin(popupPane);
+      }
 
-            try {
-                BorderPane root = FXMLLoader.load(getClass().getResource("/Views/MainPage.fxml"));
-                Opp.getPrimaryStage().getScene().setRoot(root);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-
-        public void close (ActionEvent actionEvent){
-            try {
-                Parent root = FXMLLoader.load(getClass().getResource("/Views/Login.fxml"));
-                Opp.getPrimaryStage().getScene().setRoot(root);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
+      try {
+        BorderPane root = FXMLLoader.load(getClass().getResource("/Views/MainPage.fxml"));
+        Opp.getPrimaryStage().getScene().setRoot(root);
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
     }
+  }
 
-    public void createAccount(ActionEvent actionEvent) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/Views/CreateAccount.fxml"));
-            Opp.getPrimaryStage().getScene().setRoot(root);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+  public void close(ActionEvent actionEvent) {
+    try {
+      Parent root = FXMLLoader.load(getClass().getResource("/Views/Login.fxml"));
+      Opp.getPrimaryStage().getScene().setRoot(root);
+    } catch (IOException ex) {
+      ex.printStackTrace();
     }
+  }
+
+  public void createAccount(ActionEvent actionEvent) {
+    try {
+      Parent root = FXMLLoader.load(getClass().getResource("/Views/CreateAccount.fxml"));
+      Opp.getPrimaryStage().getScene().setRoot(root);
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
 }
