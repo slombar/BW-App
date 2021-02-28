6d7553d135f11c39910d1db3faa9c737e5e56191
Luke Hoy <lahoy@wpi.edu>
2021-02-27 23:29:07 -0500

    Side Menu Updates

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/MainPageController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/MainPageController.java
@@ -1,12 +1,48 @@
 package edu.wpi.cs3733.teamO.Controllers;
 
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import edu.wpi.cs3733.teamO.Opp;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
-public class MainPageController {
+public class MainPageController implements Initializable {
+  public JFXDrawer drawer;
+  public JFXHamburger hamburger;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    // Set drawer to SideMenu
+    try {
+      VBox vbox = FXMLLoader.load(getClass().getResource("/Views/SideMenu.fxml"));
+      drawer.setSidePane(vbox);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    // transition animation of Hamburger icon
+    HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
+
+    // click event - mouse click
+    hamburger.addEventHandler(
+        MouseEvent.MOUSE_PRESSED,
+        (e) -> {
+          transition.setRate(transition.getRate() * -1);
+          transition.play();
+
+          if (drawer.isOpened()) drawer.close(); // this will close slide pane
+          else drawer.open(); // this will open slide pane
+        });
+  }
+
   public void goToNav(ActionEvent actionEvent) {
     try {
       GridPane root = FXMLLoader.load(getClass().getResource("/Views/NewNavPage.fxml"));
--- src/main/java/edu/wpi/cs3733/teamO/Controllers/SideMenuController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/SideMenuController.java
@@ -1,14 +1,18 @@
 package edu.wpi.cs3733.teamO.Controllers;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXHamburger;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 public class SideMenuController implements Initializable {
+  public JFXButton exitBtn;
   @FXML private JFXHamburger hamburgerSideBtn;
   @FXML private VBox sideVBox;
 
@@ -18,4 +22,8 @@ public class SideMenuController implements Initializable {
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {}
+
+  public void exit(ActionEvent actionEvent) {
+    Platform.exit();
+  }
 }
