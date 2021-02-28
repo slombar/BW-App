79faa2c327390b8a59aedc260f496f0e7350efcd
smspry <smspry@wpi.edu>
2021-02-27 19:19:32 -0500

    added additional ui stuff to login page

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/LoginController.java
@@ -11,23 +11,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 
 public class LoginController implements Initializable {
 
+  @FXML private BorderPane borderPane;
   @FXML private StackPane loginPane;
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
-    loginPane.setMaxSize(BorderPane.USE_PREF_SIZE, BorderPane.USE_PREF_SIZE);
+    loginPane.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
     loginPane.toBack();
   }
 
+  /**
+   * when the the circles or the images on the circle or the text below the circles are clicked, the
+   * login is pop up is prompted
+   *
+   * @param mouseEvent
+   */
   public void goToLogin(MouseEvent mouseEvent) {
     loginPane.toFront();
     PopupMaker.incompletePopup(loginPane);
   }
 
+  /**
+   * when the log in as guest button is pressed, the apllication goes directly to the main page
+   *
+   * @param actionEvent
+   */
   public void goToMain(ActionEvent actionEvent) {
     try {
       BorderPane root = FXMLLoader.load(getClass().getResource("/Views/MainPage.fxml"));
