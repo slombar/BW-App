d069d5366d48a3e28c7f2902de9f7665c7223920
Luke Hoy <lahoy@wpi.edu>
2021-02-27 22:09:32 -0500

    Hamburger

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/SideMenuController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/SideMenuController.java
@@ -1,10 +1,21 @@
 package edu.wpi.cs3733.teamO.Controllers;
 
 import com.jfoenix.controls.JFXHamburger;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
-public class SideMenuController {
-  public JFXHamburger hamburgerSideBtn;
+public class SideMenuController implements Initializable {
+  @FXML private JFXHamburger hamburgerSideBtn;
+  @FXML private VBox sideVBox;
+
+  public SideMenuController() {}
 
   public void goToMainMenu(MouseEvent mouseEvent) {}
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {}
 }
