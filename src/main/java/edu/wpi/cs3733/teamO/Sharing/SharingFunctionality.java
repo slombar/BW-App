4af10cf817517f233e1d6e8af1c19bdd6bf041e2
cindytrac <cindy.trac.11@gmail.com>
2021-03-01 01:00:44 -0500

    text images and videos!!! :)

--- src/main/java/edu/wpi/cs3733/teamO/Sharing/SharingFunctionality.java
+++ src/main/java/edu/wpi/cs3733/teamO/Sharing/SharingFunctionality.java
@@ -1,9 +1,13 @@
 package edu.wpi.cs3733.teamO.Sharing;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
 import com.vonage.client.VonageClient;
 import com.vonage.client.sms.MessageStatus;
 import com.vonage.client.sms.SmsSubmissionResponse;
 import com.vonage.client.sms.messages.TextMessage;
+import java.net.URI;
+import java.util.Arrays;
 
 public class SharingFunctionality {
 
@@ -26,9 +30,35 @@ public class SharingFunctionality {
     }
   }
 
+  public static void sendSMSTwillio(String sendingTo, String linkToFile) {
+    String ACCOUNT_SID = "ACccaa37332a0f79e457bfcb6f393b25e8";
+    String AUTH_TOKEN = "98a818e03c58110dc0fbc752695d9e40";
+
+    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    Message message =
+        Message.creator(
+                new com.twilio.type.PhoneNumber(sendingTo), //replace with cindy's number if testing
+                new com.twilio.type.PhoneNumber("+16173560972"),
+                "This is the ship that made the Kessel Run in fourteen parsecs?")
+            .setMediaUrl(Arrays.asList(URI.create("https://i.imgur.com/7KpyjV4.mp4")))
+            .create();
+
+
+    // file:/C:/users/cindy/Downloads/mapimg.png
+
+    System.out.println(message.getSid());
+  }
+
   // check email threader for previous code
   public static void sendEmailAttachment(String sendingTo, String fileToBeSent) {
     EmailThreader emailThreader = new EmailThreader(sendingTo, fileToBeSent);
     emailThreader.start();
   }
+
+  /* for testing purposes, change sendingTo param to cindy's number
+  public static void main(String[] args) {
+    sendSMSTwillio("hi", "bye");
+  }
+  */
+
 }
