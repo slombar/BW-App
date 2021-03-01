ea1cf665a63f4a28eb8173921c8600a55aa15662
Sadie Lombardi <slombardi@wpi.edu>
2021-03-01 01:57:21 -0500

    i fix dumbass person's error to make login work AGAIN

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/Popups/PatientSignInPopupController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/Popups/PatientSignInPopupController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.teamO.Database.UserHandling;
 import edu.wpi.cs3733.teamO.HelperClasses.PopupMaker;
+import edu.wpi.cs3733.teamO.HelperClasses.SwitchScene;
 import edu.wpi.cs3733.teamO.Opp;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -13,7 +14,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 
 public class PatientSignInPopupController {
@@ -36,12 +36,7 @@ public class PatientSignInPopupController {
     } else {
       try {
         UserHandling.login(username, password);
-        try {
-          BorderPane root = FXMLLoader.load(getClass().getResource("/Views/CovidSurvey.fxml"));
-          Opp.getPrimaryStage().getScene().setRoot(root);
-        } catch (IOException ex) {
-          ex.printStackTrace();
-        }
+        SwitchScene.goToParent("/Views/MainPage.fxml");
       } catch (SQLException e) {
         PopupMaker.invalidLogin(popupPane);
       }
@@ -63,12 +58,7 @@ public class PatientSignInPopupController {
    * @param actionEvent
    */
   public void createAccount(ActionEvent actionEvent) {
-    try {
-      Parent root = FXMLLoader.load(getClass().getResource("/Views/CreateAccount.fxml"));
-      Opp.getPrimaryStage().getScene().setRoot(root);
-    } catch (IOException ex) {
-      ex.printStackTrace();
-    }
+    SwitchScene.goToParent("/Views/CreateAccount.fxml");
   }
 
   public void checkEnter(KeyEvent keyEvent) {
