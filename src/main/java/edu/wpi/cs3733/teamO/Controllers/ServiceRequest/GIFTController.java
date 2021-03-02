bfc79e24685a090886e8b563132bd281d75eef58
Samuel Parks <sparks@wpi.edu>
2021-03-02 15:21:19 -0500

    fixed/deleted old TODOs

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/ServiceRequest/GIFTController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/ServiceRequest/GIFTController.java
@@ -8,6 +8,7 @@ import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.teamO.Database.RequestHandling;
+import edu.wpi.cs3733.teamO.Database.UserHandling;
 import edu.wpi.cs3733.teamO.HelperClasses.SwitchScene;
 import java.sql.Date;
 import javafx.event.ActionEvent;
@@ -36,8 +37,8 @@ public class GIFTController {
   }
 
   public void submit(ActionEvent actionEvent) {
-    // send values to DB TODO implement proper username
-    String requestedBy = getUsername();
+    // send values to DB
+    String requestedBy = UserHandling.getUsername();
     java.sql.Date dateN = Date.valueOf(dateNeeded.getValue());
     String requestType = getReqType();
     String loc = locationF.getText();
--- src/main/java/edu/wpi/cs3733/teamO/Controllers/SubmittedController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/SubmittedController.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs3733.teamO.Controllers;
 
+import edu.wpi.cs3733.teamO.HelperClasses.SwitchScene;
 import edu.wpi.cs3733.teamO.Opp;
 import java.io.IOException;
 import javafx.event.ActionEvent;
@@ -8,15 +9,12 @@ import javafx.scene.layout.AnchorPane;
 
 public class SubmittedController {
   /**
-   * Send user back to index.fxml
+   * Send user back to login page
    *
    * @param actionEvent
    * @throws IOException
    */
   public void back(ActionEvent actionEvent) throws IOException {
-    AnchorPane root = FXMLLoader.load(getClass().getResource("/Views/Archive/Index.fxml"));
-    // errors TODO: fix
-    Opp.getPrimaryStage().setFullScreen(true);
-    Opp.getPrimaryStage().getScene().setRoot(root);
+    SwitchScene.goToParent("/Views/Login.fxml");
   }
 }
