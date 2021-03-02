38669e8bffc631dd5c5273bf1b31800ec17bed73
ryandarcey <rtdarcey@wpi.edu>
2021-03-01 23:54:21 -0500

    updated some pathfinding visuals

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/ServiceRequest/MEDIController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/ServiceRequest/MEDIController.java
@@ -1,61 +1,58 @@
 package edu.wpi.cs3733.teamO.Controllers.ServiceRequest;
 
-import com.jfoenix.controls.JFXCheckBox;
+import static edu.wpi.cs3733.teamO.Controllers.ServiceRequest.RequestPageController.getReqType;
+
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.teamO.Database.RequestHandling;
 import edu.wpi.cs3733.teamO.HelperClasses.SwitchScene;
+import java.sql.Date;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-import java.sql.Date;
-
-import static edu.wpi.cs3733.teamO.Controllers.ServiceRequest.RequestPageController.getReqType;
-
 public class MEDIController {
 
-    @FXML
-    private JFXDatePicker dateNeeded;
-    @FXML private JFXTextField locationF;
-    @FXML private JFXTextArea summary;
-    @FXML private JFXTextField field1;
-    @FXML private JFXTextField field2;
+  @FXML private JFXDatePicker dateNeeded;
+  @FXML private JFXTextField locationF;
+  @FXML private JFXTextArea summary;
+  @FXML private JFXTextField field1;
+  @FXML private JFXTextField field2;
 
-    public void back(ActionEvent actionEvent) {
-        SwitchScene.goToParent("/Views/MainPage.fxml");
-    }
+  public void back(ActionEvent actionEvent) {
+    SwitchScene.goToParent("/Views/MainPage.fxml");
+  }
 
-    public void clear(ActionEvent actionEvent) {
-        locationF.clear();
-        dateNeeded.getEditor().clear();
-        summary.clear();
-        field1.clear();
-        field2.clear();
-    }
+  public void clear(ActionEvent actionEvent) {
+    locationF.clear();
+    dateNeeded.getEditor().clear();
+    summary.clear();
+    field1.clear();
+    field2.clear();
+  }
 
-    public void submit(ActionEvent actionEvent) {
-        // send values to DB TODO implement proper username
-        String requestedBy = "user"; // getUsername();
-        java.sql.Date dateN = Date.valueOf(dateNeeded.getValue());
-        String requestType = getReqType();
-        String loc = locationF.getText();
-        String sum = summary.getText();
-        String f1 = field1.getText();
-        String f2 = field2.getText();
-        String f3 = null;
+  public void submit(ActionEvent actionEvent) {
+    // send values to DB TODO implement proper username
+    String requestedBy = "user"; // getUsername();
+    java.sql.Date dateN = Date.valueOf(dateNeeded.getValue());
+    String requestType = getReqType();
+    String loc = locationF.getText();
+    String sum = summary.getText();
+    String f1 = field1.getText();
+    String f2 = field2.getText();
+    String f3 = null;
 
-        System.out.println(
-                "Adding this to DB: "
-                        + requestedBy
-                        + dateN.toString()
-                        + requestType
-                        + loc
-                        + sum
-                        + f1
-                        + f2
-                        + f3);
+    System.out.println(
+        "Adding this to DB: "
+            + requestedBy
+            + dateN.toString()
+            + requestType
+            + loc
+            + sum
+            + f1
+            + f2
+            + f3);
 
-        RequestHandling.addRequest(requestedBy, dateN, requestType, loc, sum, f1, f2, f3);
-    }
+    RequestHandling.addRequest(requestedBy, dateN, requestType, loc, sum, f1, f2, f3);
+  }
 }
--- src/main/java/edu/wpi/cs3733/teamO/GraphSystem/Graph.java
+++ src/main/java/edu/wpi/cs3733/teamO/GraphSystem/Graph.java
@@ -195,7 +195,7 @@ public class Graph {
       Circle circle = new Circle();
       // set radius to be percentage of canvas height, and bind circle's x/y to the canvas
       // width/height * percent
-      circle.radiusProperty().bind(gc.getCanvas().heightProperty().multiply(0.01));
+      circle.radiusProperty().bind(gc.getCanvas().heightProperty().multiply(0.0075));
       circle.centerXProperty().bind(gc.getCanvas().widthProperty().multiply(nXperc));
       circle.centerYProperty().bind(gc.getCanvas().heightProperty().multiply(nYperc));
 
@@ -251,6 +251,23 @@ public class Graph {
     Canvas canvas = gc.getCanvas();
     gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
+    for (Node n : path) {
+      if (n.getFloor().equals(floor) && n.getNodeType().equals("STAI")) {
+        Circle c = nodeCircleHashtable.get(n);
+        DrawHelper.drawSingleNode(gc, c, Color.GREEN);
+      } else if (n.getFloor().equals(floor) && n.getNodeType().equals("ELEV")) {
+        Circle c = nodeCircleHashtable.get(n);
+        DrawHelper.drawSingleNode(gc, c, Color.PURPLE);
+      }
+
+      if (n.getFloor().equals(floor) && n.getNodeType().equals("EXIT")) {
+        Circle c = nodeCircleHashtable.get(n);
+        DrawHelper.drawSingleNode(gc, c, Color.ORANGE);
+      }
+    }
+
+    drawMidArrows(floor);
+
     if (startNode.getFloor().equals(floor)) {
       Circle c = nodeCircleHashtable.get(startNode);
       DrawHelper.drawSingleNode(gc, c, Color.BLUE);
@@ -259,8 +276,6 @@ public class Graph {
       Circle c = nodeCircleHashtable.get(endNode);
       DrawHelper.drawSingleNode(gc, c, Color.RED);
     }
-
-    drawMidArrows(floor);
   }
 
   public void findPath(Node startNode, Node targetNode) {
