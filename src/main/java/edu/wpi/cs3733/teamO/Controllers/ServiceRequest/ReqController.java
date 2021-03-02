a2412d8717e53315651015750b811dbb5846c471
Sadie Lombardi <slombardi@wpi.edu>
2021-03-02 01:14:06 -0500

    added the submit functionality to the service request controllers and the back and the other shit (:

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/ServiceRequest/ReqController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/ServiceRequest/ReqController.java
@@ -26,7 +26,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
 public class ReqController implements Initializable {
 
