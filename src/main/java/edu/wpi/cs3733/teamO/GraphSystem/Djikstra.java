73ecd0100128ff51124c1cd643749f370396701e
sparks5115 <sparks@wpi.edu>
2021-03-07 02:23:23 -0500

    new template design pattern for A* and Djikstra

--- src/main/java/edu/wpi/cs3733/teamO/GraphSystem/Djikstra.java
+++ src/main/java/edu/wpi/cs3733/teamO/GraphSystem/Djikstra.java
@@ -1,3 +1,19 @@
 package edu.wpi.cs3733.teamO.GraphSystem;
 
-public class Djikstra extends AStarVariant {}
+import edu.wpi.cs3733.teamO.Model.Node;
+import java.util.*;
+
+public class Djikstra extends AStarVariant {
+
+  /**
+   * determines the heuristic from Node next to the target Node (must be less than the actual
+   * distance)
+   *
+   * @param next the Node from which the heuristic is calculated
+   * @param targetNode the target Node used in the heuristic calculation
+   * @return 0.0 because thats how djikstra works
+   */
+  protected double heuristic(Node next, Node targetNode) {
+    return 0.0;
+  }
+}
