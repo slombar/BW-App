742b59b540f53cc833ddfe0aea9c6ec0f08b507c
ryandarcey <rtdarcey@wpi.edu>
2021-03-07 00:46:06 -0500

    a* template pattern methods

--- src/main/java/edu/wpi/cs3733/teamO/GraphSystem/AStarSearch.java
+++ src/main/java/edu/wpi/cs3733/teamO/GraphSystem/AStarSearch.java
@@ -1,6 +1,7 @@
 package edu.wpi.cs3733.teamO.GraphSystem;
 
 import edu.wpi.cs3733.teamO.Model.Node;
+import sun.awt.image.ImageWatched;
 
 import java.util.*;
 
@@ -46,80 +47,16 @@ class AStarSearch extends AStarVariant implements AlgorithmStrategy {
      * @param targetNode destination Node of the search
      */
     public List<Node> findRoute(Node startNode, Node targetNode) {
-        for (Node n : graph.getListOfNodes()) {
-            n.setPriority(0.0);
-            n.setVisited(false);
-        }
-
-        frontier = new PriorityQueue<>();
-        cameFrom = new Hashtable<>();
-        costSoFar = new Hashtable<>();
+        init(startNode);
 
         // path, but in reverse order
         LinkedList<Node> path = new LinkedList<>();
 
-        frontier.add(startNode);
-        cameFrom.put(startNode, new Node()); // didn't come from anywhere at start
-        costSoFar.put(startNode, 0.0); // didn't cost anything at start
-
         boolean foundPath = false;
 
-        while (!frontier.isEmpty()) {
-            Node current = frontier.poll(); // continues searching from next frontier node
+        foundPath = traverse(targetNode, foundPath);
 
-            if (current.equals(targetNode)) {
-                foundPath = true;
-                break;
-            }
-
-            // iterates through current nodes neighbours
-            int llsize = current.getNeighbourList().size();
-
-            Set<Node> nList = current.getNeighbourList();
-
-            Iterator<Node> iterator = nList.iterator();
-
-            while (iterator.hasNext()) {
-                // gets next node in neighbours
-                // sets next's cost so far to current's cost so far + edge cost
-                Node next = iterator.next();
-                // TODO: change dist to get Edge length
-                double newCost = costSoFar.get(current) + dist(current, next);
-
-                // if cost to next hasn't been calculated yet, or if the newCost is less
-                //    than the previously calc'ed cost, replace with newCost
-                if (costSoFar.get(next) == null || newCost < costSoFar.get(next)) {
-                    costSoFar.put(next, newCost);
-
-                    // calculates priority (cost from start + distance to target --> lower is better)
-                    // TODO: change heuristic to take into account floor diff
-                    double priority = newCost + heuristic(next, targetNode);
-                    next.setPriority(priority);
-                    frontier.add(next);
-                    cameFrom.put(next, current); // next came from current
-                }
-            }
-        }
-
-        if (foundPath) {
-            // backtrack to add to path:
-            // start by adding target node, then iterate through cameFrom,
-            // appending next node to the front of path
-            path.add(targetNode);
-            Node cameFromNode = targetNode;
-
-            while (!cameFromNode.equals(startNode)) { // goes until it appends startNode
-                Node n = cameFrom.get(cameFromNode);
-                path.addFirst(n);
-                cameFromNode = n;
-            }
-
-            return path;
-
-        } else {
-            // TODO: throw PathNotFoundException or something
-            return null;
-        }
+        return backtrack(startNode, targetNode, foundPath);
     }
 
     /**
@@ -157,5 +94,83 @@ class AStarSearch extends AStarVariant implements AlgorithmStrategy {
         // probably can't use approximation (because calculating edge cost)
         double distSq = Math.pow(Math.abs(x1 - x2), 2.0) + Math.pow(Math.abs(y1 - y2), 2.0);
         return Math.sqrt(distSq);
+    }
+
+    // methods for 'template pattern':
+
+    private void init(Node startNode) {
+        for (Node n : graph.getListOfNodes()) {
+            n.setPriority(0.0);
+            n.setVisited(false);
+        }
+
+        frontier = new PriorityQueue<>();
+        cameFrom = new Hashtable<>();
+        costSoFar = new Hashtable<>();
+
+        frontier.add(startNode);
+        cameFrom.put(startNode, new Node()); // didn't come from anywhere at start
+        costSoFar.put(startNode, 0.0); // didn't cost anything at start
+    }
+
+    private boolean traverse(Node targetNode, boolean foundPath) {
+        while (!frontier.isEmpty()) {
+            Node current = frontier.poll(); // continues searching from next frontier node
+
+            if (current.equals(targetNode)) {
+                foundPath = true;
+                return foundPath;
+            }
+
+            // iterates through current nodes neighbours
+            int llsize = current.getNeighbourList().size();
+
+            Set<Node> nList = current.getNeighbourList();
+
+            for (Node next : nList) {
+                // gets next node in neighbours
+                // sets next's cost so far to current's cost so far + edge cost
+                // TODO: change dist to get Edge length
+                double newCost = costSoFar.get(current) + dist(current, next);
+
+                // if cost to next hasn't been calculated yet, or if the newCost is less
+                //    than the previously calc'ed cost, replace with newCost
+                if (costSoFar.get(next) == null || newCost < costSoFar.get(next)) {
+                    costSoFar.put(next, newCost);
+
+                    // calculates priority (cost from start + distance to target --> lower is better)
+                    // TODO: change heuristic to take into account floor diff
+                    double priority = newCost + heuristic(next, targetNode);
+                    next.setPriority(priority);
+                    frontier.add(next);
+                    cameFrom.put(next, current); // next came from current
+                }
+            }
+        }
+        return foundPath;
+    }
+
+    private LinkedList<Node> backtrack(Node startNode, Node targetNode, boolean foundPath) {
+        LinkedList<Node> path = new LinkedList<>();
+
+        if (foundPath) {
+            // backtrack to add to path:
+            // start by adding target node, then iterate through cameFrom,
+            // appending next node to the front of path
+            path.add(targetNode);
+            Node cameFromNode = targetNode;
+
+            while (!cameFromNode.equals(startNode)) { // goes until it appends startNode
+                Node n = cameFrom.get(cameFromNode);
+                path.addFirst(n);
+                cameFromNode = n;
+            }
+
+            return path;
+
+        } else {
+            // TODO: throw PathNotFoundException or something
+            return null;
+        }
     }
 }
