c063c4be993624e484de80471c953a59af245c83
sparks5115 <sparks@wpi.edu>
2021-03-07 18:39:42 -0500

    polyline shit

--- src/main/java/edu/wpi/cs3733/teamO/Maps/Directions.java
+++ src/main/java/edu/wpi/cs3733/teamO/Maps/Directions.java
@@ -2,9 +2,10 @@ package edu.wpi.cs3733.teamO.Maps;
 
 import com.google.maps.*;
 import com.google.maps.errors.ApiException;
-import com.google.maps.model.DirectionsResult;
-import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 // Reference
 // PlaceDetailsRequest query = PlacesApi.placeDetails(new
@@ -27,6 +28,10 @@ public class Directions {
             "100 Institute Rd. Worcester MA, 01609",
             "182 Russel Street Worcester MA, 01609");
 
+    dirReq.departureTimeNow();
+    dirReq.units(Unit.IMPERIAL);
+    dirReq.mode(TravelMode.DRIVING);
+
     DirectionsResult result = null;
 
     try {
@@ -45,9 +50,42 @@ public class Directions {
     assert result != null;
     DirectionsRoute[] route = result.routes;
 
-
     for (DirectionsRoute r : route) {
-      System.out.println(r.toString());
+      System.out.println(r.overviewPolyline.getEncodedPath());
+      System.out.println(decodePoly(r.overviewPolyline.getEncodedPath()));
     }
+  }
+
+  private static List<LatLng> decodePoly(String encoded) {
+
+    List<LatLng> poly = new ArrayList<LatLng>();
+    int index = 0, len = encoded.length();
+    int lat = 0, lng = 0;
+
+    while (index < len) {
+      int b, shift = 0, result = 0;
+      do {
+        b = encoded.charAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      lat += dlat;
+
+      shift = 0;
+      result = 0;
+      do {
+        b = encoded.charAt(index++) - 63;
+        result |= (b & 0x1f) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      lng += dlng;
+
+      LatLng p = new LatLng((((double) lat / 1E5)), (((double) lng / 1E5)));
+      poly.add(p);
+    }
+
+    return poly;
   }
 }
