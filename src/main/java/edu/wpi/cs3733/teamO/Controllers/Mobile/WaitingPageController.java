34f24181bc241cec6429fd6be323e21bf4895b45
smspry <smspry@wpi.edu>
2021-03-07 23:13:28 -0500

    added foundation for popups in waiting page

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/Mobile/WaitingPageController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/Mobile/WaitingPageController.java
@@ -2,13 +2,24 @@ package edu.wpi.cs3733.teamO.Controllers.Mobile;
 
 import com.jfoenix.controls.JFXButton;
 import edu.wpi.cs3733.teamO.HelperClasses.SwitchScene;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.StackPane;
 
-public class WaitingPageController {
+public class WaitingPageController implements Initializable {
   @FXML private JFXButton hospitalNavBtn;
   @FXML private StackPane popupNotification;
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    hospitalNavBtn.setDisable(true);
+    // TODO: need to use the following popups
+    //    PopupMaker.mainEntranceNotif(popupNotification);
+    //    PopupMaker.covidEntranceNotif(popupNotification);
+  }
 
   /**
    * go back to the covid 19 survey that will be sent ot the hospital to review entry request
