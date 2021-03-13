d39fad17ec8313d1e9018a17a199d56ae4efc860
sparks5115 <sparks@wpi.edu>
2021-03-12 23:01:16 -0500

    credits page now exists

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/AboutController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/AboutController.java
@@ -2,17 +2,30 @@ package edu.wpi.cs3733.teamO.Controllers;
 
 import edu.wpi.cs3733.teamO.Database.UserHandling;
 import edu.wpi.cs3733.teamO.HelperClasses.SwitchScene;
+import edu.wpi.cs3733.teamO.Opp;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
 
 public class AboutController {
 
-
-    public void goToHome(ActionEvent actionEvent) {
-        if (UserHandling.getEmployee()) {
-            SwitchScene.goToParent("/Views/StaffMainPage.fxml");
-        } else {
-            SwitchScene.goToParent("/Views/MainPage.fxml");
-        }
+  public void goToHome(ActionEvent actionEvent) {
+    if (UserHandling.getEmployee()) {
+      SwitchScene.goToParent("/Views/StaffMainPage.fxml");
+    } else {
+      SwitchScene.goToParent("/Views/MainPage.fxml");
     }
+  }
+
+  public void goToCreditsPage(MouseEvent mouseEvent) {
+    SwitchScene.goToParent("/Views/CreditsPage.fxml");
+  }
+
+  public void handCursor(MouseEvent mouseEvent) {
+    Opp.getPrimaryStage().getScene().setCursor(Cursor.HAND); // Change cursor to hand
+  }
+
+  public void normalCursor(MouseEvent mouseEvent) {
+    Opp.getPrimaryStage().getScene().setCursor(Cursor.DEFAULT); // Change cursor to normal arrow
+  }
 }
