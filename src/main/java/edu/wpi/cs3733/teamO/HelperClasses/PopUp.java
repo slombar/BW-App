f68a922db653cb42c7791f489eba2019717148a5
Sadie Lombardi <slombardi@wpi.edu>
2021-03-13 13:57:33 -0500

    popup

--- src/main/java/edu/wpi/cs3733/teamO/HelperClasses/PopUp.java
+++ src/main/java/edu/wpi/cs3733/teamO/HelperClasses/PopUp.java
@@ -1,42 +1,55 @@
 package edu.wpi.cs3733.teamO.HelperClasses;
 
-import edu.wpi.cs3733.teamO.Controllers.Revamped.NavController;
+import java.awt.*;
 import javafx.event.EventHandler;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.WindowEvent;
 
-import java.awt.event.MouseAdapter;
+/*
+  // alignList = new ArrayList<>();
+        selectedNode = clickedNode;
+
+       PopUp nodePop = new PopUp();
+
+        // create window event
+      EventHandler<WindowEvent> event =
+          new EventHandler<WindowEvent>() {
+             public void handle(WindowEvent e) {
+               // TODO highlight node you are changing
+             }
+          };
+
+       // add event
+      nodePop.setShowingProperties(event);
+   }
+
+    draw();
+     System.out.println("mapCanvas click");
+ */
 
 public class PopUp {
+  // create a menu
+  ContextMenu contextMenu;
 
-        public PopUp() {
+  public PopUp() {
 
-            // create a menu
-            ContextMenu contextMenu = new ContextMenu();
+    // create a menu
+    contextMenu = new ContextMenu();
 
-            // create menuitems
-            MenuItem menuItem1 = new MenuItem("menu item 1");
-            MenuItem menuItem2 = new MenuItem("menu item 2");
-            MenuItem menuItem3 = new MenuItem("menu item 3");
+    // create menuitems
+    MenuItem menuItem1 = new MenuItem("edit");
+    MenuItem menuItem2 = new MenuItem("delete");
+    MenuItem menuItem3 = new MenuItem("add edge");
 
-            // add menu items to menu
-            contextMenu.getItems().add(menuItem1);
-            contextMenu.getItems().add(menuItem2);
-            contextMenu.getItems().add(menuItem3);
+    // add menu items to menu
+    contextMenu.getItems().add(menuItem1);
+    contextMenu.getItems().add(menuItem2);
+    contextMenu.getItems().add(menuItem3);
+  }
 
-            // create window event
-            EventHandler<WindowEvent> event = new EventHandler<WindowEvent>() {
-                public void handle(WindowEvent e)
-                {
-                    //TODO highlight node you are changing
-                }
-            };
-
-            // add event
-            contextMenu.setOnShowing(event);
-            contextMenu.setOnHiding(event);
-        }
-
+  public void setShowingProperties(EventHandler event) {
+    // add event
+    contextMenu.setOnShowing(event);
+    contextMenu.setOnHiding(event);
+  }
 }
