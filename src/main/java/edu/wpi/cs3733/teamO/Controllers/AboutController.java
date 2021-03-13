7db866764df7e3d8d531356ea3a718d154bc6c87
sparks5115 <sparks@wpi.edu>
2021-03-12 19:06:56 -0500

    about page updated

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/AboutController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/AboutController.java
@@ -1,13 +1,18 @@
 package edu.wpi.cs3733.teamO.Controllers;
 
+import edu.wpi.cs3733.teamO.Database.UserHandling;
 import edu.wpi.cs3733.teamO.HelperClasses.SwitchScene;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 
 public class AboutController {
-  public Button backBtn;
 
-  public void back(ActionEvent actionEvent) {
-    SwitchScene.goToParent("/Views/MainPage.fxml");
-  }
+
+    public void goToHome(ActionEvent actionEvent) {
+        if (UserHandling.getEmployee()) {
+            SwitchScene.goToParent("/Views/StaffMainPage.fxml");
+        } else {
+            SwitchScene.goToParent("/Views/MainPage.fxml");
+        }
+    }
 }
