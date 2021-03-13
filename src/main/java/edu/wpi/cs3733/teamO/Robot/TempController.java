854e3ffef3a553d3d4505ff02e8951a434425c33
Sadie Lombardi <slombardi@wpi.edu>
2021-03-13 16:12:49 -0500

    remove bad repo and comment out bad code

--- src/main/java/edu/wpi/cs3733/teamO/Robot/TempController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Robot/TempController.java
@@ -1,41 +1,43 @@
 package edu.wpi.cs3733.teamO.Robot;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import me.aflak.arduino.Arduino;
 
 public class TempController extends Thread implements Initializable {
 
   public Label tempLabel;
   private static String port = (new SPortScan()).getActivePort(); // <--- get the port
   // create connection to arduino
-  private static Arduino ArduinoCon = new Arduino(port, 9600);
+  //private static Arduino ArduinoCon = new Arduino(port, 9600);
 
   // create connection to adruino, check the com port in adruino IDE and paste it instead "COM8"
-  private static Arduino AdruinoCon = new Arduino("COM5", 9600);
+ // private static Arduino AdruinoCon = new Arduino("COM5", 9600);
 
   public void retry(ActionEvent actionEvent) {
     // AdruinoCon.serialWrite(this.commands[commandIndex]); // pick a command from an array and send
     // it to USB
-    String arduinoOutput = AdruinoCon.serialRead();
+    //String arduinoOutput = AdruinoCon.serialRead();
     int count = 0;
-    for (char c : arduinoOutput.toCharArray()) {
+    char arduinoOutput[] = null;
+    for (char c : arduinoOutput) {
       System.out.println("Character " + count + " " + c);
       count++;
     }
   }
 
   public void submit(ActionEvent actionEvent) {
-    AdruinoCon.closeConnection();
+    //AdruinoCon.closeConnection();
   }
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
 
-    AdruinoCon.openConnection();
+    //AdruinoCon.openConnection();
+
     // whenever connection is estabilished the adruino is restarted, we need to wait for it
     // otherwise it wont listen
     try {
