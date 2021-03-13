beb91d677d685417f3b2014c84d992d197bdc5f6
cindytrac <cindy.trac.11@gmail.com>
2021-03-13 13:29:07 -0500

    about page attempt 2

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/LoginController.java
@@ -112,6 +112,5 @@ public class LoginController implements Initializable {
     SwitchScene.newWindowParent("/Views/MobileApp/MainScreen.fxml");
   }
 
-    public void goToTemp(ActionEvent actionEvent) {
-    }
+  public void goToTemp(ActionEvent actionEvent) {}
 }
