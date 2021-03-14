dddaac8ec1df9d4f56f796962bd702575ab57eb5
smspry <42656055+smspry@users.noreply.github.com>
2021-03-14 16:54:44 -0400

    foundation for new phone main screen

--- src/main/java/edu/wpi/cs3733/teamO/Controllers/RevampedMobile/MainMobileScreenController.java
+++ src/main/java/edu/wpi/cs3733/teamO/Controllers/RevampedMobile/MainMobileScreenController.java
@@ -3,24 +3,17 @@ package edu.wpi.cs3733.teamO.Controllers.RevampedMobile;
 import javafx.event.ActionEvent;
 
 public class MainMobileScreenController {
-    public void goToNav(ActionEvent actionEvent) {
-    }
+  public void goToNav(ActionEvent actionEvent) {}
 
-    public void goToAbout(ActionEvent actionEvent) {
-    }
+  public void goToAbout(ActionEvent actionEvent) {}
 
-    public void goToGoogleNav(ActionEvent actionEvent) {
-    }
+  public void goToGoogleNav(ActionEvent actionEvent) {}
 
-    public void goToParking(ActionEvent actionEvent) {
-    }
+  public void goToParking(ActionEvent actionEvent) {}
 
-    public void gotToLogin(ActionEvent actionEvent) {
-    }
+  public void gotToLogin(ActionEvent actionEvent) {}
 
-    public void goToProfile(ActionEvent actionEvent) {
-    }
+  public void goToProfile(ActionEvent actionEvent) {}
 
-    public void goToLogin(ActionEvent actionEvent) {
-    }
+  public void goToLogin(ActionEvent actionEvent) {}
 }
