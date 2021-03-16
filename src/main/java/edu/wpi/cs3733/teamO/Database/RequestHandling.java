b7af8d5dff5102d926fda5f4eb1698982d900d1a
Sadie Lombardi <slombardi@wpi.edu>
2021-03-15 23:06:09 -0400

    FIX SOMES TUFFF INSIDE OF REQUEST CONTROLLER AND IN REQUEST HANDLING TO BE GOOD

--- src/main/java/edu/wpi/cs3733/teamO/Database/RequestHandling.java
+++ src/main/java/edu/wpi/cs3733/teamO/Database/RequestHandling.java
@@ -10,8 +10,43 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class RequestHandling {
+  /**
+   * Get the request from the database based off of the ID
+   *
+   * @param reqID
+   * @return
+   */
+  public static Request getRequest(int reqID) {
+    Request r = new Request();
 
-  // TODO: add function to set employee assigned
+    try {
+      String query = "SELECT * FROM SRS WHERE REQUESTID = ?";
+      PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement(query);
+      ps.setInt(1, reqID);
+
+      ResultSet rset = ps.executeQuery();
+      rset.next();
+
+      // add properties to the node
+      r.setRequestID(reqID);
+      r.setRequestedBy(rset.getString("PERSON"));
+      r.setFulfilledBy(rset.getString("ASSIGNED"));
+      r.setDateRequested(rset.getDate("DATECREATED"));
+      r.setDateNeeded(rset.getDate("DATENEEDED"));
+      r.setRequestType(rset.getString("REQUESTTYPE"));
+      r.setLocationNodeID(rset.getString("LOCATION"));
+      r.setSummary(rset.getString("SUMMARY"));
+
+      rset.close();
+      ps.close();
+
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    }
+
+    return r;
+  }
+
 
   /**
    * Retrieve all service requests from database, depending on their type
@@ -38,7 +73,7 @@ public class RequestHandling {
       }
 
     } else {
-      query = "SELECT * FROM SRS WHERE REQTYPE = ?";
+      query = "SELECT * FROM SRS WHERE REQUESTTYPE = ?";
       try {
         // database statement to grab values
         pstmt = DatabaseConnection.getConnection().prepareStatement(query);
@@ -63,17 +98,17 @@ public class RequestHandling {
 
       // grab everything from the result set and add to observable list for processing
       while (rset.next()) {
-        reqID = rset.getInt("requestID");
-        requestedBy = rset.getString("requestedBy");
-        fulfilledBy = rset.getString("fulfilledBy");
-        dateRequested = rset.getDate("dateRequested");
-        dateNeeded = rset.getDate("dateNeeded");
-        requestType = rset.getString("reqtype");
-        location = rset.getString("location");
-        summary = rset.getString("summary");
+        reqID = rset.getInt("ID");
+        requestedBy = rset.getString("PERSON");
+        fulfilledBy = rset.getString("ASSIGNED");
+        dateRequested = rset.getDate("DATECREATED");
+        dateNeeded = rset.getDate("DATENEEDED");
+        requestType = rset.getString("REQUESTTYPE");
+        location = rset.getString("LOCATION");
+        summary = rset.getString("SUMMARY");
 
-        Request req =
-            new Request(
+        //requests generate here
+        Request req = new Request(
                 reqID,
                 requestedBy,
                 fulfilledBy,
@@ -82,24 +117,6 @@ public class RequestHandling {
                 requestType,
                 location,
                 summary);
-
-        System.out.println(
-            "Retrieved this from Services: "
-                + reqID
-                + ", "
-                + requestedBy
-                + ", "
-                + fulfilledBy
-                + ", "
-                + dateRequested.toString()
-                + ", "
-                + dateNeeded.toString()
-                + ", "
-                + requestType
-                + ", "
-                + location
-                + ", "
-                + summary);
 
         requestList.add(req);
       }
@@ -128,15 +145,13 @@ public class RequestHandling {
 
     pstmt =
         DatabaseConnection.getConnection()
-            .prepareStatement("SELECT * FROM Requests WHERE REQUESTID = ?");
+            .prepareStatement("SELECT STATUS FROM Requests WHERE ID = ?");
 
     pstmt.setInt(1, reqID);
 
     ResultSet rset = pstmt.executeQuery();
-
-    while (rset.next()) {
-      status = rset.getString("STATUS");
-    }
+    rset.next();
+    status = rset.getString("STATUS");
 
     rset.close();
     pstmt.close();
@@ -150,53 +165,17 @@ public class RequestHandling {
    * @param reqID
    */
   public static void setStatus(int reqID, String status) throws SQLException {
-    String query = "UPDATE REQUESTS SET STATUS = '" + status + "' WHERE REQUESTID = ?";
+    String query = "UPDATE SRS SET STATUS = ? WHERE REQUESTID = ?";
 
     PreparedStatement pstmt = null;
     pstmt = DatabaseConnection.getConnection().prepareStatement(query);
 
-    pstmt.setInt(1, reqID);
+    pstmt.setString(1, status);
+    pstmt.setInt(2, reqID);
     pstmt.executeUpdate();
     pstmt.close();
   }
 
-  /**
-   * Get the request from the database based off of the ID
-   *
-   * @param reqID
-   * @return
-   */
-  public static Request getRequest(int reqID) {
-    Request r = new Request();
-
-    try {
-      PreparedStatement pstmt =
-          DatabaseConnection.getConnection()
-              .prepareStatement("SELECT * FROM Requests WHERE REQUESTID = ?");
-      pstmt.setInt(1, reqID);
-
-      ResultSet rset = pstmt.executeQuery();
-      rset.next();
-
-      // add properties to the node
-      r.setRequestID(reqID);
-      r.setRequestedBy(rset.getString("requestedBy"));
-      r.setFulfilledBy(rset.getString("fulfilledBy"));
-      r.setDateRequested(rset.getDate("dateRequested"));
-      r.setDateNeeded(rset.getDate("dateNeeded"));
-      r.setRequestType(rset.getString("reqType"));
-      r.setLocationNodeID(rset.getString("location"));
-      r.setSummary(rset.getString("summary"));
-
-      rset.close();
-      pstmt.close();
-
-    } catch (SQLException throwables) {
-      throwables.printStackTrace();
-    }
-
-    return r;
-  }
 
   public static void addRequest(Request r) {
     // get current date
@@ -229,7 +208,7 @@ public class RequestHandling {
    * @param requestID
    */
   public static void deleteRequest(int requestID) {
-    String query = "DELETE FROM REQUESTS WHERE REQUESTID = ?";
+    String query = "DELETE FROM SRS WHERE REQUESTID = ?";
 
     try {
       PreparedStatement preparedStmt = null;
@@ -245,51 +224,32 @@ public class RequestHandling {
   }
 
   /**
-   * TODO Finish this Allows editing of request
-   *
-   * @param requestID
-   * @param fulfilledBy
-   * @param requestType
-   * @param locationNodeID
-   * @param summary
-   * @param customParameter1
-   * @param customParameter2
-   * @param customParameter3
+   * Edit the request in the database
+   * Updates the request type, summary, location for this specific request
+   * @param r
    */
-  public static void editRequest(
-      int requestID,
-      String fulfilledBy,
-      String requestType,
-      String locationNodeID,
-      String summary,
-      String customParameter1,
-      String customParameter2,
-      String customParameter3) {
-
-    summary += customParameter1 + customParameter2 + customParameter3;
+  public static void editRequest(Request r) {
 
     String query =
-        "UPDATE REQUESTS SET fulfilledBy = ?, "
-            + "reqType = ?, location = ?, summary = ? WHERE requestID = ?";
+        "UPDATE SRS SET REQUESTTYPE = ?, SUMMARY = ?, LOCATION = ? WHERE requestID = ?";
     try {
       PreparedStatement preparedStmt = null;
       preparedStmt = DatabaseConnection.getConnection().prepareStatement(query);
-      preparedStmt.setString(1, fulfilledBy);
-      preparedStmt.setString(2, requestType);
-      preparedStmt.setString(3, locationNodeID);
-      preparedStmt.setString(4, summary);
-      preparedStmt.setInt(5, requestID);
+      preparedStmt.setString(1, r.getRequestType());
+      preparedStmt.setString(2, r.getSummary());
+      preparedStmt.setString(3, r.getLocationNodeID());
+      preparedStmt.setInt(4, r.getRequestID());
 
       preparedStmt.executeUpdate();
       preparedStmt.close();
-      System.out.println("printed " + query);
+
     } catch (SQLException throwables) {
       throwables.printStackTrace();
     }
   }
 
   public static void updateRequest(int requestID, String locationNode) {
-    String query = "UPDATE REQUESTS SET location = ? WHERE requestID = ?";
+    String query = "UPDATE SRS SET LOCATION = ? WHERE ID = ?";
     try {
       PreparedStatement preparedStmt = null;
       preparedStmt = DatabaseConnection.getConnection().prepareStatement(query);
@@ -297,7 +257,6 @@ public class RequestHandling {
       preparedStmt.setInt(2, requestID);
       preparedStmt.executeUpdate();
       preparedStmt.close();
-      System.out.println("printed " + query);
       WaitingPageController.setEntrance(locationNode);
       //      WaitingPageController.notifyUser();
 
