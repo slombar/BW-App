e0d00b9910ae3d392e108faf0aef43fe7176b62d
smspry <42656055+smspry@users.noreply.github.com>
2021-03-16 13:33:10 -0400

    added info to login page

--- src/main/java/edu/wpi/cs3733/teamO/DinoGame/UserInterface.java
+++ src/main/java/edu/wpi/cs3733/teamO/DinoGame/UserInterface.java
@@ -1,58 +1,48 @@
 package edu.wpi.cs3733.teamO.DinoGame;
 
-import edu.wpi.cs3733.teamO.DinoGame.components.Dino;
-import edu.wpi.cs3733.teamO.DinoGame.components.Ground;
-import edu.wpi.cs3733.teamO.DinoGame.components.Obstacles;
 import edu.wpi.cs3733.teamO.Opp;
-import javafx.embed.swing.JFXPanel;
-import javafx.event.EventHandler;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import lombok.SneakyThrows;
-
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
 import javax.imageio.*;
 import javax.swing.*;
-import java.util.*;
+import lombok.SneakyThrows;
 
 public class UserInterface extends Pane {
   JFrame mainWindow = new JFrame("T-Rex Run");
-  Stage mainStage = Opp.getPrimaryStage(); //or make new stage
+  Stage mainStage = Opp.getPrimaryStage(); // or make new stage
 
   public static int WIDTH = 800;
   public static int HEIGHT = 500;
 
   public void createAndShowGUI() throws IOException {
-    mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //    mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    mainStage.setOnCloseRequest(event -> {
-      event.consume();
-    });
+    mainStage.setOnCloseRequest(
+        event -> {
+          event.consume();
+        });
 
-
-    //build scene from pane
-    Container container = mainWindow.getContentPane();
+    // build scene from pane
+    //    Container container = mainWindow.getContentPane();
     Parent root =
-            FXMLLoader.load(getClass().getResource("/RevampedViews/DesktopApp/SignInPage.fxml"));
+        FXMLLoader.load(getClass().getResource("/RevampedViews/DesktopApp/SignInPage.fxml"));
     Scene scene = new Scene(root);
-    //Scene scene = new Scene(root, 600, 400);
-
-
+    // Scene scene = new Scene(root, 600, 400);
 
     GamePanel gamePanel = new GamePanel();
-    gamePanel.addKeyListener(gamePanel);
-    gamePanel.setFocusable(true);
+    //    gamePanel.addKeyListener(gamePanel);
+    //    gamePanel.setFocusable(true);
 
-
-    //just a borderpane
-    container.setLayout(new BorderLayout());
-    container.add(gamePanel, BorderLayout.CENTER);
+    // just a borderpane
+    //    container.setLayout(new BorderLayout());
+    //    container.add(gamePanel, BorderLayout.CENTER);
 
     BorderPane border = new BorderPane();
     AnchorPane anchor = new AnchorPane();
@@ -60,33 +50,32 @@ public class UserInterface extends Pane {
     VBox vbox = new VBox();
 
     border.setTop(hbox);
+    //    anchor.getChildren().add(new ImageView(gamePanel));
 
-    //anchor.getChildren().add(gamePanel); //HOW TO PASS IN GAMEPANEL
-    //vbox.getChildren().add(gamePanel);
+    // anchor.getChildren().add(gamePanel); //HOW TO PASS IN GAMEPANEL
+    // vbox.getChildren().add(gamePanel);
 
+    // public GamePanel() {
+    //      WIDTH = UserInterface.WIDTH;
+    //      HEIGHT = UserInterface.HEIGHT;
+    //
+    //      ground = new Ground(HEIGHT);
+    //      dino = new Dino();
+    //      obstacles = new Obstacles((int) (WIDTH * 1.5));
+    //
+    //      score = 0;
+    //
+    //      setSize(WIDTH, HEIGHT);
+    //      setVisible(true);
+    ////    }
 
-// public GamePanel() {
-//      WIDTH = UserInterface.WIDTH;
-//      HEIGHT = UserInterface.HEIGHT;
-//
-//      ground = new Ground(HEIGHT);
-//      dino = new Dino();
-//      obstacles = new Obstacles((int) (WIDTH * 1.5));
-//
-//      score = 0;
-//
-//      setSize(WIDTH, HEIGHT);
-//      setVisible(true);
-////    }
-
-    mainWindow.setSize(WIDTH, HEIGHT);
-    mainWindow.setResizable(false);
-    mainWindow.setVisible(true);
+    //    mainWindow.setSize(WIDTH, HEIGHT);
+    //    mainWindow.setResizable(false);
+    //    mainWindow.setVisible(true);
 
     mainStage.setMaxWidth(WIDTH);
     mainStage.setMaxHeight(HEIGHT);
     mainStage.setResizable(false);
-
   }
 
   public static void main(String[] args) {
